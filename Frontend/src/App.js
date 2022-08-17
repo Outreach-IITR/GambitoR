a36a1607738aa5012de8886e5c666e751f05315b
Akshit <Gakshit92@gmail.com>
2022-08-17 21:28:27 +0530

    File changes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,19 @@
 import React from "react";
-import ReactDOM from "react";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import LandingPage from "./Components/LandingPage/LandingPage";
-import NavBar from "./HelperComponents/Navbar/Navbar";
 import Footer from "./HelperComponents/Footer/Footer";
+import Navbar from "./HelperComponents/NavBar/NavBar"
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<NavBar />}>
-            <Route index element={<LandingPage />} />
-          </Route>
-          <Route path="/about" element={<NavBar />}>
+          <Route path="/" element={<Navbar />}>
             <Route index element={<LandingPage />} />
           </Route>
         </Routes>
       </Router>
-      <Footer />
     </>
   );
 }
--- Frontend/src/HelperComponents/NavBar/NavBar.js
+++ Frontend/src/HelperComponents/NavBar/NavBar.js
@@ -1,41 +1,46 @@
 import React from "react";
-import styles from "./Navbar.module.css";
+import styles from "./NavBar.module.css";
 import navLogo from "./assets/navLogo.png";
 import topRightImg from "./assets/topRightImg.png";
 import topLeftImg from "./assets/topLeftImg.png";
-
 import RegButton from "../../Components/RegButton/RegButton";
+import { Link, Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div className={styles.mainNav}>
-      <img className={styles.leftImg} src={topLeftImg} />
+    <>
+      <div className={styles.mainNav}>
+        <img className={styles.leftImg} src={topLeftImg} alt="" />
 
-      <img className={styles.navLogo} alt="img" src={navLogo} />
-      <ul className={styles.navList}>
-        <li>
-          <a class={styles.rightNavItems} href="#">
-            About
-          </a>
-        </li>
-        <li>
-          <a class={styles.rightNavItems} href="#">
-            Events
-          </a>
-        </li>
-        <li>
-          <a class={styles.rightNavItems} href="#">
-            Contacts
-          </a>
-        </li>
-        <li>
-          <a class={styles.rightNavItems} href="#">
-            <RegButton />
-          </a>
-        </li>
-      </ul>
-      <img className={styles.rightImg} src={topRightImg}></img>
-    </div>
+        <img className={styles.navLogo} alt="img" src={navLogo} />
+        <ul className={styles.navList}>
+          <li>
+            <Link class={styles.rightNavItems} to="#">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link class={styles.rightNavItems} to="#">
+              Events
+            </Link>
+          </li>
+          <li>
+            <Link class={styles.rightNavItems} to="#">
+              Contacts
+            </Link>
+          </li>
+          <li>
+            <Link class={styles.rightNavItems} to="#">
+              <RegButton />
+            </Link>
+          </li>
+        </ul>
+        <img alt="" className={styles.rightImg} src={topRightImg} />
+      </div>
+      <Outlet />
+      <Footer />
+    </>
   );
 }
 
