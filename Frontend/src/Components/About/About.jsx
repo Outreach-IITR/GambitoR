0f67692b8780e114c41f3f3513c2dcd048878392
Rahul Modi <rahul_km@ee.iitr.ac.in>
2022-08-19 01:27:23 +0530

    Categories component created

---
+++ Frontend/src/Components/About/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import RoundComp from "./RoundComp";
+
+import style from "./About.module.css";
+import Header from "../../HelperComponents/Header/Header";
+import Categories from "./Categories";
+
+const headDescription = (
+  <>
+    {`IIT Roorkee is glad to announce the first edition of GambitoR- the National
+    School Championship where only creative thinking and quick wit can guide you
+    to victory.`}
+    <br />
+    {`Test yourselves against a comprehensive exam designed by our
+    studentcommunity, that will call into question your logical and analytical
+    skills.Compete with the best minds of the nation and win a chance to visit a
+    major scientific & technical institute and a free pass to Thomso’22 (for
+    subsequent rounds).`}
+  </>
+);
+
+const About = () => {
+  return (
+    <>
+      <Header headerText="ABOUT US" descriptionText={headDescription} />
+      <div className={style.AboutContainer}>
+        <RoundComp />
+        <Categories />
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ Frontend/src/Components/About/Categories.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { useState } from "react";
+
+import categoryIcon from "./Assets/categoryicon.png";
+
+import style from "./Categories.module.css";
+
+const categoriesData = [
+  {
+    id: 1,
+    name: "METIOX",
+    description: (
+      <>
+        {"for Class"}
+        <br /> {"9th & 10th"}
+      </>
+    ),
+  },
+  {
+    id: 2,
+    name: "APOLLOX",
+    description: (
+      <>
+        {"for Class"}
+        <br /> {"11th"}
+      </>
+    ),
+  },
+  {
+    id: 3,
+    name: "ATHENOX",
+    description: (
+      <>
+        {"for Class"}
+        <br /> {"12th"}
+      </>
+    ),
+  },
+];
+
+const Categories = () => {
+  const [active, setActive] = useState(1);
+
+  const onClickActiveHandler = (idx) => {
+    setActive(idx);
+  };
+
+  return (
+    <div>
+      <h1 className={style.headingTitle}>CATEGORIES</h1>
+      <h3 className={style.headingLine}>
+        The competition will be conducted in{" "}
+        <span>Three Different Categories</span>
+      </h3>
+      <div className={style.categoriesContainer}>
+        {categoriesData.map((obj, idx) => {
+          return (
+            <div
+              key={idx}
+              className={style.categoriesBox}
+              onClick={() => onClickActiveHandler(idx)}
+            >
+              <div
+                className={`${style.categoriesUnactiveContent} ${
+                  active === idx ? style.active : ""
+                }`}
+              >
+                <h4>{obj.name}</h4>
+                <p>{obj.description}</p>
+              </div>
+              <div
+                className={`${style.categoriesActiveContent} ${
+                  active === idx ? style.active : ""
+                }`}
+              >
+                <img src={categoryIcon} alt="" />
+                <h4>{obj.name}</h4>
+                <p>{obj.description}</p>
+              </div>
+              <div
+                className={`${style.activeBar} ${
+                  active === idx ? style.active : ""
+                }`}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
