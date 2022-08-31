dbd51a4a08cc0da7a00f98ad3e57cba39b28cceb
Akshit <Gakshit92@gmail.com>
2022-08-31 15:43:16 +0530

    adding env frontend

--- Frontend/src/https/api.js
+++ Frontend/src/https/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
+import env from "react-dotenv";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/",
-  'Content-type': 'application/json',
-  Accept: 'application/json',
+  baseURL: env.REACT_APP_API_URL,
+  "Content-type": "application/json",
+  Accept: "application/json",
   withCredentials: true,
-})
+});
 
 export default api;
