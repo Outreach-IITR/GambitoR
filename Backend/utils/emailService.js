73e90ccd98dad8aed7b121c342269a5f81e0f093
Akshit <Gakshit92@gmail.com>
2022-08-31 16:38:38 +0530

    email service update

--- Backend/utils/emailService.js
+++ Backend/utils/emailService.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const mailService = nodemailer.createTransport({
-  service: "gmail",
+  service: process.env.EMAIL_SERVICE,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
