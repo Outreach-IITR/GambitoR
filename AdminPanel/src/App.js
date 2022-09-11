32301ae238615f2f712b8b40f27dd7bea0fb0b57
Akshit <Gakshit92@gmail.com>
2022-09-11 16:09:23 +0530

    Admin panel updateds

--- AdminPanel/src/App.js
+++ AdminPanel/src/App.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 import Dashboard from "./Components/Dashboard/Dashboard";
+import Login from "./Components/Login/Login";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
+  const [login, setLogin] = useState(false);
+
   return (
     <div className="App">
       <Navbar />
-      <Dashboard />
+      {
+        login ?
+          <Dashboard /> : <Login />
+      }
     </div>
   );
 }
