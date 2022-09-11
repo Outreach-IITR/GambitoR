8ef3bdc43c16ecff7f38912974b5c819434ad465
Rahul Modi <rahul_km@ee.iitr.ac.in>
2022-09-11 15:46:56 +0530

    Navbar added

--- AdminPanel/src/Components/Dashboard/Dashboard.jsx
+++ AdminPanel/src/Components/Dashboard/Dashboard.jsx
@@ -3,14 +3,7 @@ import React from "react";
 import styles from "./Dashboard.module.css";
 
 const Dashboard = () => {
-  return (
-    <div>
-      <nav className={styles.Navbar}>
-        <h1 className={styles.title}>Dashboard</h1>
-        <h1 className={styles.logo}>GambitoR</h1>
-      </nav>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Dashboard;
