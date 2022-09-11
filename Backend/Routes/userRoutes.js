03d93fdca0a8dcd43a0ee0f7fac8c53645897b66
Akshit <Gakshit92@gmail.com>
2022-09-11 16:03:51 +0530

    signup route close

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { signup, login } = require('../Controller/authController');
 const router = express.Router();
 
-router.route('/signup').post(signup);
+// router.route('/signup').post(signup);
 router.route('/login').post(login);
 
 module.exports = router;
