79a2cce7a45854742825b4275ebd953992f4f04b
Pushpendra Pal <94526347+Pushpendra100@users.noreply.github.com>
2022-09-11 22:36:02 +0530

    download button added

---
+++ AdminPanel/src/Components/Dashboard/downloadButton/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./Button.module.css"
+
+const Button = () => {
+  return (
+    <button className={styles.downloadButton}>Download</button>
+  )
+}
+
+export default Button;
