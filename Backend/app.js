4ed59e7d0084963cdf71cc57a6b7c2ba6752d862
Akshit <Gakshit92@gmail.com>
2022-09-14 17:23:45 +0530

    Adding another env

--- Backend/app.js
+++ Backend/app.js
@@ -1,60 +1,61 @@
-const express = require('express')
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const cors = require('cors');
-const globalErrorHandler = require('./Controller/errorController');
-const registrationRoutes = require('./Routes/registrationRoutes');
-const AppError = require('./utils/appError');
-const path = require('path');
-const { protect } = require('./Controller/authController');
-const userRoutes = require('./Routes/userRoutes');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+const globalErrorHandler = require("./Controller/errorController");
+const registrationRoutes = require("./Routes/registrationRoutes");
+const AppError = require("./utils/appError");
+const path = require("path");
+const { protect } = require("./Controller/authController");
+const userRoutes = require("./Routes/userRoutes");
 
 dotenv.config();
 
 const app = express();
 
-const allowList = [process.env.ALLOWED_URL_1, process.env.ALLOWED_URL_2];
-
+const allowList = [
+  process.env.ALLOWED_URL_1,
+  process.env.ALLOWED_URL_2,
+  process.env.ALLOWED_URL_3,
+];
 
 var corsOptionsDelegate = function (req, callback) {
   var corsOptions = {
     credentials: true,
   };
 
-
-  if (allowList.indexOf(req.header('Origin')) !== -1) {
-    corsOptions.origin = true  // reflect (enable) the requested origin in the CORS response
+  if (allowList.indexOf(req.header("Origin")) !== -1) {
+    corsOptions.origin = true; // reflect (enable) the requested origin in the CORS response
   } else {
-    corsOptions.origin = false // disable CORS for this request
+    corsOptions.origin = false; // disable CORS for this request
   }
 
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 
 app.use(express.json());
 app.use(cors(corsOptionsDelegate));
 
 app.use("/api/public", protect, express.static("uploads"));
 
-console.log(process.env.NODE_ENV)
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
-app.use('/test', (req, res) => {
-  res.send('Working')
+app.use("/test", (req, res) => {
+  res.send("Working");
 });
 
 app.use(express.json({ limit: "8mb" }));
 
 //All the routes comes here
-app.use('/api/v1/registration', registrationRoutes)
-app.use('/api/v1/user', userRoutes);
+app.use("/api/v1/registration", registrationRoutes);
+app.use("/api/v1/user", userRoutes);
 
-app.use('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
+app.use("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
-
 
 //This is the global error handler
 app.use(globalErrorHandler);
