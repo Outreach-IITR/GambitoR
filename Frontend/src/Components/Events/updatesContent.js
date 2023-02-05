4623d3883b7cb4b3def0ba406d6b0178e9c9852a
Rahul Modi <rahul_km@ee.iitr.ac.in>
2023-02-05 15:55:48 +0530

    Result date announced

--- Frontend/src/Components/Events/updatesContent.js
+++ Frontend/src/Components/Events/updatesContent.js
@@ -2,21 +2,25 @@ const updates = [
   {
     id: 1,
     description:
-      "The inception round of GambitoR is scheduled on 5th February 2023.",
-    // time: "TIME",
-    // date: "DATE",
+      "The inception round results will be announced on 12th February 2023. ",
   },
   {
     id: 2,
-    description: "Registration deadline: 2nd February 2023.",
-    // time: "TIME",
-    // date: "DATE",
+    description:
+      "Answer key of inception round will be released on 7th February 2023.",
   },
   {
     id: 3,
+    description:
+      "The inception round of GambitoR is scheduled on 5th February 2023.",
+  },
+  {
+    id: 4,
+    description: "Registration deadline: 2nd February 2023.",
+  },
+  {
+    id: 5,
     description: "Mock questions have been released.",
-    // time: "TIME",
-    // date: "DATE",
   },
 ];
 
