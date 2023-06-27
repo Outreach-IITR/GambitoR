f0e1409472e87bd07a9865bc068f9397c4fd54b1
Pratham Goel <prathamgoel01102003@gmail.com>
2023-06-27 14:45:17 +0530

    PC version complete

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,9 +4,10 @@ import React from "react";
 function Navbar(){
      return(
         <div className="navbar">
-        
-           <img src="images/Frame.png" className="img1" alt="logo" />
-           <img src="images/GAMBITOR.png" className="img2" alt="title" />
+        <div className="logo">
+            <img src="images/Frame.png" className="img1" alt="logo" />
+            <img src="images/GAMBITOR.png" className="img2" alt="title" />
+        </div>
            <button className="btn btn-outline">Download Brochure</button>
         </div>
      )
